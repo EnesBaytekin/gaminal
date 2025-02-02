d747dadbef654e30612519113d132c9b72a52f91
EnesBaytekin <enesbaytekin38@gmail.com>
2025-02-02 18:23:01 +0300

    chore: print screen smoothly

--- gaminal/screen.py
+++ gaminal/screen.py
@@ -1,12 +1,17 @@
 from gaminal.image import Image
-from gaminal.util import clear_terminal
+from sys import stdout
 
 class Screen(Image):
     def __init__(self, width=80, height=24):
         super().__init__(width, height)
+        self.last_data = [[" " for _ in range(height)] for _ in range(width)]
     def print(self):
-        clear_terminal()
+        MOVE_CURSOR = "\033[{row};{col}H"
+        stdout.write("\033[2J")
         for y in range(self.height):
             for x in range(self.width):
-                print(self.data[x][y], end="")
-            print()
+                if self.data[x][y] != self.last_data[x][y]:
+                    stdout.write(MOVE_CURSOR.format(row=y, col=x))
+                    stdout.write(self.data[x][y])
+        stdout.write(MOVE_CURSOR.format(row=self.height, col=0))
+        stdout.flush()
